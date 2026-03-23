61e40afc7054b1cbb917b734a0ea64d60812f194
Steli <p2333762@gmail.com>
2026-03-23 22:54:49 +0000

    def update_or_append_onboarding

--- app.py
+++ app.py
@@ -10715,8 +10715,8 @@ def _render_onboarding_page(display_name, role, csrf, existing, msg, msg_ok, typ
           <input class="input {bad('birth')}" type="date" name="birth" value="{escape(val('birth', 'BirthDate'))}">
 
           <label class="sub {bad_label('phone_num')}" style="margin-top:10px; display:block;">Phone Number</label>
-<input type="hidden" name="phone_cc" value="">
-<input class="input {bad('phone_num')}" name="phone_num" value="{escape(val('phone_num', 'PhoneNumber'))}">
+          <input type="hidden" name="phone_cc" value="">
+          <input class="input {bad('phone_num')}" name="phone_num" value="{escape(val('phone_num', 'PhoneNumber'))}">
           </div>
 
           <h2 style="margin-top:14px;">Address</h2>
@@ -10738,8 +10738,8 @@ def _render_onboarding_page(display_name, role, csrf, existing, msg, msg_ok, typ
           </div>
 
           <label class="sub {bad_label('ec_phone')}" style="margin-top:10px; display:block;">Emergency Contact Phone</label>
-<input type="hidden" name="ec_cc" value="">
-<input class="input {bad('ec_phone')}" name="ec_phone" value="{escape(val('ec_phone', 'EmergencyContactPhoneNumber'))}">
+          <input type="hidden" name="ec_cc" value="">
+          <input class="input {bad('ec_phone')}" name="ec_phone" value="{escape(val('ec_phone', 'EmergencyContactPhoneNumber'))}">
           </div>
 
           <h2 style="margin-top:14px;">Medical</h2>
